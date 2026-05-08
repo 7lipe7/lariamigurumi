33f606aeb808233e44d83bebc03def6803afe2fd
Felipe Santana <fsantanalopes3@gmail.com>
2026-05-08 20:58:41 -0300

    lazy

--- js/catalago.js
+++ js/catalago.js
@@ -96,3 +96,18 @@ botaoPersonalisados.addEventListener("click", () => {
 });
 
 
+const imagens = document.querySelectorAll('.lazy');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      img.classList.add('loaded');
+      observer.unobserve(img);
+    }
+  });
+});
+
+imagens.forEach(img => observer.observe(img));
+
--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 
-document.getElementById("ano").textContent = new Date().getFullYear();
 
+
+const imagens = document.querySelectorAll('.lazy');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      img.classList.add('loaded');
+      observer.unobserve(img);
+    }
+  });
+});
+
+imagens.forEach(img => observer.observe(img));
